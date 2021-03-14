e3a4900a05b68e9f221eab3b08f7df8c1122ff62
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-13 17:50:15 -0700

    attempting dropdown of patient ids

--- static/app.js
+++ static/app.js
@@ -1,3 +1,23 @@
 const bellyData = "./data/samples.json"
 
-d3.json(bellyData).then(data => console.log(data));
+var patients = d3.json(bellyData).then(data => data.names);
+
+var dropDown = d3.select("#selDataset");
+
+var patientList = dropDown.append("ul");
+
+for (i = 0; i < patients.length; i++) {
+    patientList.append("li").text(pat[i]);
+}
+
+
+// var data = [{
+//     type: 'bar',
+//     x: [20, 14, 23],
+//     y: ['giraffes', 'orangutans', 'monkeys'],
+//     orientation: 'h'
+//   }];
+  
+//   Plotly.newPlot('myDiv', data);
+
+
