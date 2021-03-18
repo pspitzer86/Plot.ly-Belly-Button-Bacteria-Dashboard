773847b9792c85fab018cbd1bead7a82b1709daf
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-17 19:48:04 -0700

    commented

--- static/app.js
+++ static/app.js
@@ -1,18 +1,32 @@
+// Created a constant for the file path to the data json to be read in.
+
 const BellyData = "./data/samples.json";
+
+// Function called when the dropdown of patient ids is changed to a different patient id.  First thing that occurs is reading in the data
 
 function optionChanged(newId) {
     d3.json(BellyData).then((importedData) => {
 
+        // Setting a variable for the metadata and samples portions of the json.
+
         var demo = importedData.metadata;
         var samples = importedData.samples;
+
+        // Function that sets the patient id chosen from the dropdown as the one to be filtered out of the dataset.
 
         function filteredIds(data) {
             return data.id == newId;
         }
 
+        // Metadata variable filtered by the patient id selected from the dropdown first, then the necessary id is grabbed from the html using d3
+
         var filteredMeta = demo.filter(filteredIds);
 
         var demoBox = d3.select("#sample-metadata");
+
+        // The html is cleared and a table is appended in the html id, the keys are put into an array, each one is looped through to append a 
+        // table row that is then appended with a td that contains all the necessary demographic data from the metadata portion of the json.
+        //  This displays the demographics of each patient id in the correct box in the html.
 
         demoBox.html("");
         var addBox = demoBox.append("table");
@@ -26,11 +40,17 @@ function optionChanged(newId) {
             row.append("td").text((`${key}: ${filteredMeta[0][key]}`));
         });
 
+        // Samples variable filtered by the patient id selected from the dropdown, the otu id, otu label, and sample value data are set to their 
+        // own variables.  All three of these should be a list of equal length for each patient id.
+
         var filterSamp = samples.filter(filteredIds);
 
         var otuIds = filterSamp[0].otu_ids;
         var otuLabels = filterSamp[0].otu_labels;
         var sampleValues = filterSamp[0].sample_values;
+
+        // An empty list is created, one of the three lists is looped through its length to create a dictionary list of all three variables
+        // so they are all accurately correlated to one another for plotting.
 
         var sampleArray = [];
 
@@ -42,10 +62,16 @@ function optionChanged(newId) {
             sampleArray.push(sampleDict);
         }
 
+        // The dictionary list is sorted in descending order, then sliced to grab the first 10 since the bar graph needs to show the top 10
+        // OTUs for each patient.  The top 10 are then reversed since Plotly defaults from the top down.
+
         var sortSamp = sampleArray.sort((a,b) => b.sample_value - a.sample_value);
 
         var sliceSamp = sortSamp.slice(0,10);
         var reversedSamp = sliceSamp.reverse();
+
+        // First trace created for the bar graph, using the reversed dictionary list of the top ten OTUs for each patient id, organized as a
+        // horizontal bar graph.  Layout created to have a title, set the size, and label axes.
 
         var trace1 = {
             x: reversedSamp.map(values => values.sample_values),
@@ -73,7 +99,12 @@ function optionChanged(newId) {
             showlegend: false
         };
 
+        //  Plotting the bar graph bar calling the bar id from the html and using the trace, set to data, and the layout created.
+
         Plotly.newPlot("bar", data, layout);
+
+        // Second trace created for the bubble chart using the values from the variables of the otu ids, otu labels, and their sample values.
+        // The size of the markers is set to the sample values for each otu id.  Layout created to set title, size, and axes labels.
 
         var trace2 = {
             mode: "markers",
@@ -102,11 +133,16 @@ function optionChanged(newId) {
             showlegend: false
         };
 
+        // Plotting the bubble chart calling the bubble id from the html and using the second trace, set to data2, and the second layout.
+
         Plotly.newPlot("bubble", data2, layout2);
 
     });
 }
 
+// Reading in the data json using the d3 library, setting the names portion of the data to a variable, grabbing the dropdown id from the html,
+// looping through the length of the names variable and appending the patient ids to the dropdown, then calling the function above on the selected
+// patient id from the dropdown.
 
 d3.json(BellyData).then((importedData) => {
 
